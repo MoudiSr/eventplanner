1b65dccf5de1f2086bca01afd0485e40f07e3014
Mhmmd Sr <mhmmdsrouji@gmail.com>
2025-11-25 11:00:34 +0200

    fix

--- components/children-session.tsx
+++ components/children-session.tsx
@@ -1,12 +1,10 @@
 'use client'
 import { SessionProvider } from "next-auth/react";
-import { usePathname } from "next/navigation";
 import NavBar from "./navbar"
 
 const ChildrenSession = ({ children }: {
     children: React.ReactNode;
 }) => {
-    const pathname = usePathname();
 
     const wrapperClass = "min-h-screen bg-gradient-to-b from-[#05060a] via-[#0b1220] to-[#0a1a2f] text-white";
 
