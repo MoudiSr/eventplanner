24099dec41145b9ff8114e66420d9dac0841bc85
Mohammad Srouji <40596420+MoudiSr@users.noreply.github.com>
2026-01-08 01:17:26 +0200

    Infer better-auth client types

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -1,6 +1,9 @@
 import { createAuthClient } from "better-auth/react";
+import { InferAuth } from "better-auth/client";
 import { usernameClient } from "better-auth/client/plugins";
+import type { auth } from "@/lib/auth";
 
 export const authClient = createAuthClient({
+    $InferAuth: InferAuth<typeof auth>(),
     plugins: [usernameClient()],
 });
